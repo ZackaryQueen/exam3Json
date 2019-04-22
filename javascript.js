e601b56956546ed607d2e59da3916e5696567f01
Zackary Queen <zq0895074@otc.edu>
2019-04-22 13:03:41 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,19 @@
+// $.ajax ({ url: thepath, method: "GET"})
+// .success(function (response) {
+//    var json = $.parseJSON (response);
+// });
+const jsonfile = require('jsonfile')
+const file = 'data.json'
+
+
+jsonfile.readFile(file, function (err, obj) {
+  if (err) console.error(err)
+  console.dir(obj)
+})
+
+var fname = {
+    name: "Kali",
+    age: "19",
+}
+
+// console.log(fname);
